37f7a8c35d006cd4d4a4adbbcd97b9404cc036ed
zhangtianpu <582528987@qq.com>
2021-09-22 15:33:19 +0800

    add figure

--- main_app.py
+++ main_app.py
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 if __name__ == '__main__':
     """数据读取，及数据处理，标准化"""
     # data=np.load(my_parameter.RAW_PATH)['data'][:,0,:]
-    # data=data_process.scal_data(data)
+    data=data_process.scal_data(data)
     # data_df=pd.DataFrame(data,columns=['traffic_flow','share','speed'])
     # data_df=data_process.get_history_data(data_df,columns=['traffic_flow','share','speed'])
 
@@ -76,9 +76,9 @@ if __name__ == '__main__':
     """
     evaluation.load_model(my_parameter.GRU_MODEL_PATH,GRU_model)
     GRU_pred = GRU_model.predict(lstm_test_X_torch)
-    # gru_val_mse, gru_val_r2 = evaluation.evaluate(lstm_test_y_torch, GRU_pred)
-    # print("-----------GRU---------\nmse:%s\tr2:%s\n--------------------------------\n" % (
-    # gru_val_mse, gru_val_r2))
+    gru_val_mse, gru_val_r2 = evaluation.evaluate(lstm_test_y_torch, GRU_pred)
+    print("-----------GRU---------\nmse:%s\tr2:%s\n--------------------------------\n" % (
+    gru_val_mse, gru_val_r2))
     #
     # GRU_re=np.hstack([test_y, GRU_pred])
     # re=pd.DataFrame(GRU_re,columns=['traffic_flow','share','speed','GRU_traffic_flow_pred','GRU_share_pred','GRU_speed_pred'])
@@ -95,8 +95,8 @@ if __name__ == '__main__':
     evaluation.load_model(my_parameter.GRU_ATTENTION_PATH,GRU_attention_model)
     GRU_attention_pred = GRU_attention_model.predict(lstm_test_X_torch)
     # alpha=GRU_attention_model.get_alpha()
-    # gru_attention_val_mse, gru_attention_val_r2 =evaluation.evaluate(lstm_test_y_torch,GRU_attention_pred)
-    # print("-----------GRU_attention---------\nmse:%s\tr2:%s\n--------------------------------\n" % (gru_attention_val_mse, gru_attention_val_r2))
+    gru_attention_val_mse, gru_attention_val_r2 =evaluation.evaluate(lstm_test_y_torch,GRU_attention_pred)
+    print("-----------GRU_attention---------\nmse:%s\tr2:%s\n--------------------------------\n" % (gru_attention_val_mse, gru_attention_val_r2))
     # GRU_attention_re=np.hstack([test_y, GRU_attention_pred])
     # re=pd.DataFrame(GRU_attention_re,columns=['traffic_flow','share','speed','GRU_attention_traffic_flow_pred','GRU_attention_share_pred','GRU_attention_speed_pred'])
     # re.to_csv(my_parameter.GRU_ATTENTION_RE_PATH,index=False)
@@ -121,8 +121,8 @@ if __name__ == '__main__':
     evaluation.load_model(my_parameter.GRU_DUAL_STAGE_ATTENTION_PATH,GRU_dual_stage_attention_model)
     GRU_dual_stage_attention_pred = GRU_dual_stage_attention_model.predict(lstm_test_X_torch)
     # alpha=GRU_dual_stage_attention_model.get_alpha()
-    # GRU_dual_stage_attention_val_mse, GRU_dual_stage_attention_val_r2 =evaluation.evaluate(lstm_test_y_torch,GRU_dual_stage_attention_pred)
-    # print("-----------GRU_dual_stage_attention---------\nmse:%s\tr2:%s\n--------------------------------\n" % (GRU_dual_stage_attention_val_mse, GRU_dual_stage_attention_val_r2))
+    GRU_dual_stage_attention_val_mse, GRU_dual_stage_attention_val_r2 =evaluation.evaluate(lstm_test_y_torch,GRU_dual_stage_attention_pred)
+    print("-----------GRU_dual_stage_attention---------\nmse:%s\tr2:%s\n--------------------------------\n" % (GRU_dual_stage_attention_val_mse, GRU_dual_stage_attention_val_r2))
     # GRU_dual_stage_attention_re=np.hstack([test_y, GRU_dual_stage_attention_pred])
     # re=pd.DataFrame(GRU_dual_stage_attention_re,columns=['traffic_flow','share','speed','GRU_dual_stage_attention_traffic_flow_pred','GRU_dual_stage_attention_share_pred','GRU_dual_stage_attention_speed_pred'])
     # re.to_csv(my_parameter.GRU_DUAL_STAGE_ATTENTION_RE_PATH,index=False)
@@ -151,9 +151,8 @@ if __name__ == '__main__':
     """
     evaluation.load_model(my_parameter.TCN_MODEL_PATH,TCN_attention_model)
     TCN_attention_pred = TCN_attention_model.predict(lstm_test_X_torch)
-    print(TCN_attention_pred)
-    # TCN_val_mse,TCN_val_r2 =evaluation.evaluate(lstm_test_y_torch,TCN_attention_pred)
-    # print("-----------TCN_attention---------\nrmse:%s\tr2:%s\n--------------------------------\n" % (TCN_val_mse, TCN_val_r2))
+    TCN_val_mse,TCN_val_r2 =evaluation.evaluate(lstm_test_y_torch,TCN_attention_pred)
+    print("-----------TCN_attention---------\nrmse:%s\tr2:%s\n--------------------------------\n" % (TCN_val_mse, TCN_val_r2))
     # TCN_attention_re=np.hstack([test_y, TCN_attention_pred])
     #re=pd.DataFrame(TCN_attention_re,columns=['traffic_flow','share','speed','TCN_attention_traffic_flow_pred','TCN_attention_share_pred','TCN_attention_speed_pred'])
     #re.to_csv(my_parameter.TCN_ATTENTION_RE_PATH,index=False)
@@ -163,20 +162,20 @@ if __name__ == '__main__':
     """
     各个模型对比图
     """
-    # plt.rcParams['font.sans-serif'] = ['SimSun']
-    # plt.rcParams['font.family'] = 'SimHei'
-    # time_range=30
-    # show_feature=0
-    # time=np.arange(0,time_range).astype('str')
-    # plt.plot(time,lstm_pred[:time_range,show_feature],label='lstm_pred')
-    # plt.plot(time,test_y[:time_range,show_feature],label='ground_truth',color='black')
-    # plt.plot(time,GRU_pred[:time_range,show_feature],label='gru_pred')
-    # plt.plot(time,GRU_dual_stage_attention_pred[:time_range,show_feature],label='gru_dual_stage_attention')
-    # plt.plot(time, GRU_attention_pred[:time_range, show_feature], label='gru_attention')
-    # plt.legend()
-    # plt.xlabel('time')
-    # plt.ylabel('traffic flow')
-    # plt.show()
+    plt.rcParams['font.sans-serif'] = ['SimSun']
+    plt.rcParams['font.family'] = 'SimHei'
+    time_range=30
+    show_feature=0
+    time=np.arange(0,time_range).astype('str')
+    plt.plot(time,lstm_pred[:time_range,show_feature],label='lstm_pred')
+    plt.plot(time,test_y[:time_range,show_feature],label='ground_truth',color='black')
+    plt.plot(time,GRU_pred[:time_range,show_feature],label='gru_pred')
+    plt.plot(time,GRU_dual_stage_attention_pred[:time_range,show_feature],label='gru_dual_stage_attention')
+    plt.plot(time, GRU_attention_pred[:time_range, show_feature], label='gru_attention')
+    plt.legend()
+    plt.xlabel('time')
+    plt.ylabel('traffic flow')
+    plt.savefig('./fig/traffic flow prediction with different models.jpg')
 
     """
     损失函数
